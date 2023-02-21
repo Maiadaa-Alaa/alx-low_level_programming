c99eb62867ac11f1c1664176ea5e06688c1dfd73
maiadaAlaa <maiadaalaa8@gmail.com>
2023-02-21 11:21:07 -0800

    4-isalpha.c
    git commit -m 4-isalpha.c
    
    git push
    
    git commit -m 4-isalpha.c
    git commit -m 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
-
-	/**
-	 * _isalpha - checks for alphabetic character
-	 * @d: the character to be checked
-	 * Return: 1 if d is a letter, 0 otherwise
-	 */
-	int _isalpha(int d)
-	{
-		return ((d >= 'a' && d <= 'z') || (d >= 'A' && d <= 'Z'));
-	}
-
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+	int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
