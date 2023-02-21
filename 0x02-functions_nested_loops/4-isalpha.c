b3b26b14f33e8fa320f40ce7f84437e84bb5dfc7
maiadaAlaa <maiadaalaa8@gmail.com>
2023-02-21 11:15:09 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,8 @@
 
 	/**
 	 * _isalpha - checks for alphabetic character
-	 * @c: the character to be checked
-	 * Return: 1 if c is a letter, 0 otherwise
+	 * @d: the character to be checked
+	 * Return: 1 if d is a letter, 0 otherwise
 	 */
 	int _isalpha(int d)
 	{
