e3b708b15254753532af2d08c187e6f1566dccac
maiadaAlaa <maiadaalaa8@gmail.com>
2023-02-21 11:43:44 -0800

    9-times_table.c
    git commit -m 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,32 @@
 	 */
 	void times_table(void)
 	{
-		int i, j, m;
+		int i, j, k;
 
 
 		for (i = 0; i < 10; i++)
 		{
 			for (j = 0; j < 10; j++)
 			{
-				m = j * i;
+				k = j * i;
 				if (j == 0)
 				{
-					_putchar(m + '0');
+					_putchar(k + '0');
 				}
 
 
-				if (m < 10 && j != 0)
+				if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (m >= 10)
+				} else if (k >= 10)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
